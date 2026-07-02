e088a366fa303e68ff2b82199f9019cda33389b4
p1nkGh0st <angelsclover1212@gmail.com>
2026-07-02 13:40:24 -0700

    test: add env var support for global config

--- config.example.py
+++ config.example.py
@@ -4,11 +4,13 @@
 Instructions: Copy this file, rename it to 'config.py', and fill in your details.
 """
 
+import os
+
 # Discord Webhook URLs
-DISCORD_WEBHOOK_URL = "XXXXXXXXXXXXXXXXXXX" # Replace with your discord webhook URL
+DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL", "XXXXXXXXXXXXXXXXXXX") # Replace with your discord webhook URL
 
 # Crypto API endpoint for BTC, ETH, and XRP price and 24h change data
-CRYPTO_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,ripple&vs_currencies=usd&include_24hr_change=true"
+CRYPTO_URL = os.environ.get("CRYPTO_URL", "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,ripple&vs_currencies=usd&include_24hr_change=true")
 
 # Scheduler Settings (Default)
-SCHEDULED_TIME = "14:00"
+SCHEDULED_TIME = os.environ.get("SCHEDULED_TIME", "10:00")
