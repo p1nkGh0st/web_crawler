f888ab9dda881d7eaa5a576e09cfdecd176a1a5f
p1nkGh0st <angelsclover1212@gmail.com>
2026-07-02 13:33:14 -0700

    server test

--- config.example.py
+++ config.example.py
@@ -11,4 +11,4 @@ DISCORD_WEBHOOK_URL = "XXXXXXXXXXXXXXXXXXX" # Replace with your discord webhook 
 CRYPTO_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,ripple&vs_currencies=usd&include_24hr_change=true"
 
 # Scheduler Settings (Default)
-SCHEDULED_TIME = "10:00"
+SCHEDULED_TIME = "14:00"
