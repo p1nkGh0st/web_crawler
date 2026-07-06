52c5f7e914e182383836e78c21be3206305dcfb6
p1nkGh0st <angelsclover1212@gmail.com>
2026-07-06 15:12:03 -0700

    Chore: untrack config.py and enforce gitignore

---
+++ config.example.py
@@ -0,0 +1,5 @@
+#config.example.py
+
+DISCORD_WEBHOOK_URL = "XXXXXXXXXXXXXXXXXXX" # Replace with your discord webhook
+CRYPTO_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,ripple&vs_currencies=usd&include_24hr_change=true"
+SCHEDULED_TIME = "10:00"
--- config.py
+++ config.py
@@ -5,10 +5,10 @@ Keep your sensitive credentials and fixed settings here.
 """
 
 # Discord Webhook URLs
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1520178279129022546/XyyIHJeFxfyCW_jaMznXHBAkTDI4zc7FdBoQ0BHkL2FDTUIzI8SsafJeMnO2eIBVgp-Y"
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1523807626616639590/CfoSKL_oYWR2jqWMz-AiFoEMXqrV8f1k8ffJzZuNcLyk6BalTz6P8qEkdBKHK1XrN8pP"
 
 # Crypto API endpoint for BTC, ETH, and XRP price and 24h change data
 CRYPTO_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,ripple&vs_currencies=usd&include_24hr_change=true"
 
 # Scheduler Settings
-SCHEDULED_TIME = "14:00"
+SCHEDULED_TIME = "15:05"
