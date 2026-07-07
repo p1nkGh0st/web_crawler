d1d2c79179bb23c5d6359954ad60517ada3df2c8
p1nkGh0st <angelsclover1212@gmail.com>
2026-07-06 17:07:45 -0700

    Chore: add CRYPTO_API_KEY environment variable to config

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ import os
 
 DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
 CRYPTO_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,ripple&vs_currencies=usd&include_24hr_change=true"
+CRYPTO_API_KEY = os.environ.get("CRYPTO_API_KEY")
 SCHEDULED_TIME = os.environ.get("SCHEDULED_TIME", "14:00")
